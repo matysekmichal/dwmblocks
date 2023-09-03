4b0419c44b081f991349da5063636cd0bf8859c6
Michał Matysek <hello@michalmatysek.com>
2023-09-03 20:48:20 +0200

    improved formatting

--- blocks.def.h
+++ blocks.def.h
@@ -1,19 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/	    /*Command*/	          /*Update Interval*/   /*Update Signal*/
-//  {"",            "sb-music",	          560,	                22},
-//  {"vol: ",       "sb-volume",	      560,		            10},
-//  {"trc: ",       "sb-timew",	          5,	                25},
-//  {"bt: ",        "sb-bluetooth",       0,		            11},
-//	{"rss: ",       "sb-rss",	          0,	                21},
-//  {"doc: ",       "sb-docker",	      0,		            12},
-//  {"net: ",       "sb-traffic",         5,     	            18},
-//  {"con: ",       "sb-internet",        10,     	            28},
-//  {"dsc: ",       "sb-disk /home",      180,	                13},
-//  {"mem: ",       "sb-mem",             10,                   15},
-//  {"cpu: ",       "sb-cpu",             4,     	            19},
-//  {"btt: ",       "sb-battery",         120,     	            27},
-//  {"",            "sb-clock",           5,		            20},
+    /*Icon*/	    /*Command*/	            /*Update Interval*/ /*Update Signal*/
+//  {"",            "sb-music",             560,                22},
+//  {"",            "sb-volume",            560,                10},
+//  {"",            "sb-timew",             5,                  25},
+//  {"",            "sb-bluetooth",         0,                  11},
+//  {"",            "sb-rss",               0,                  21},
+//  {"",            "sb-docker",            0,                  12},
+//  {"",            "sb-internet",          0,                  28},
+//  {"",            "sb-traffic",           5,                  18},
+//  {"",            "sb-disk /home",        180,                13},
+//  {"",            "sb-mem",               10,                 15},
+//  {"",            "sb-cpu",               4,                  19},
+//  {"",            "sb-battery",           120,                27},
+//  {"",            "sb-clock",             5,                  20},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
